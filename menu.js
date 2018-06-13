cae8244c6ebf6888d9bedeba935418988bd7f7f7
marina_sakovich <sakovichm@mail.ru>
2018-06-13 17:29:39 +0300

    rename open-menu in side-menu

---
+++ menu.js
@@ -0,0 +1,27 @@
+(function () {
+    let buttonMenu = document.querySelector('.menu__button-menu');
+    buttonMenu.addEventListener('click', clickButtonMenuHandler);
+
+    function clickButtonMenuHandler() {
+        let openMenu = document.querySelector('.side-menu');
+        openMenu.classList.toggle('side-menu--visible');
+        openMenu.addEventListener('click', clickOpenMenuHandler);
+
+        // let openMenuContent = document.querySelector('.side-menu__content');
+        // openMenuContent.classList.toggle('side-menu__content--opened');
+    }
+
+    function clickOpenMenuHandler() {
+
+        if(!event.target.closest('.side-menu__content')) {
+            let openMenu = document.querySelector('.side-menu');
+            openMenu.classList.toggle('side-menu--visible');
+
+            openMenu.removeEventListener('click', clickOpenMenuHandler);
+
+            // let openMenuContent = document.querySelector('.open-menu__content');
+            // openMenuContent.classList.toggle('side-menu__content--opened');
+        }
+    }
+
+}());
